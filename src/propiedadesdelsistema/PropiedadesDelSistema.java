dd95c93c2df3d18905b5f48ff40955f60682047b
Rider <rider@Rider.lan>
2012-10-12 20:02:01 -0500

    Interfaz Grafica Agregada

--- src/propiedadesdelsistema/PropiedadesDelSistema.java
+++ src/propiedadesdelsistema/PropiedadesDelSistema.java
@@ -4,10 +4,8 @@
  */
 package propiedadesdelsistema;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
